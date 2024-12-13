e1774eca32780f2c3c9075e47a1086b85d957624
Alessandro Henrique <ale.dev.github@gmail.com>
2024-12-13 11:30:48 -0300

    Update desafio008.py

--- python_ex/desafio008.py
+++ python_ex/desafio008.py
@@ -1,4 +1,4 @@
 metro=float(input('Digite um valor em metros para saber a conversão em centímetro e milímetro: '))
 print('Você digitou {} metros'.format(metro), end=' >>> ')
-print('{} metros em centímetros é {} centímetros'.format(metro, metro*1000), end=' >>> ')
-print('{} metros em milímetros é {} milímetros'.format(metro, metro*10000), end=' >>> ')
+print('{} metros em centímetros é {} centímetros'.format(metro, metro*100), end=' >>> ')
+print('{} metros em milímetros é {} milímetros'.format(metro, metro*1000), end=' >>> ')
